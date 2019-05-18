3352b353e0aac7c4486d422ca9414d1399603b0d
CarlAllmann <49835729+CarlAllmann@users.noreply.github.com>
2019-05-18 18:53:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+'use strict'
+
+$(function() {
+$(".welcome").show(); 
+$(".search").hide(); 
+$(".results").hide();
+ 
+console.log('App loaded! Waiting for submit!');
+
+});
+
+$("#start").click(function(){
+    $(".welcome").hide(); 
+    $(".search").show();  
+    console.log("start clicked"); 
+})
+
+$("#go-search").click(function(){
+  $(".welcome").hide(); 
+  $(".search").hide();
+  $(".results").show(); 
+  watchForm();
+  console.log("go button clicked"); 
+})
+
+$("#restart-button").click(function(event) {
+    $(".welcome").hide(); 
+    $(".search").show(); 
+    $(".results").hide();
+    console.log("re-started"); 
+  })
+
+
+
+  function getImage(dogBreed) {
+  fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
+  .then(response => {
+  if (response.ok) {
+      return response.json();
+  }
+  throw new Error(response.statusText);
+  })
+  .then(responseJson => generateImage(responseJson))
+  .catch(err => {
+  $('#js-error-message').text(`Something went wrong: ${err.message} I want the error message to go here`);
+  });
+
+  };
+
+
+
+function generateImage(responseJson) {
+  $(".search").hide();  
+  $(".results").show(); 
+  $(".dog-images").html("");
+  $('.dog-images').append(`<img src="${responseJson.message}" class="dog-images" alt="Your Dog Breed">`)
+  console.log(responseJson);
+};
+  
+
+
+function watchForm(event) {
+  $("#dog-form").submit(event => {
+  event.preventDefault();
+  let dogBreed = $(".dog-selector").val();
+  getImage(dogBreed);
+  console.log("input sent");
+  });
+}
+
